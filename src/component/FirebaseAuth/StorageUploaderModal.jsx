0053ee978cab2b86c2237ee3cd28b0d85d974612
VladRyb <vla9d5@mail.ru>
2020-06-16 10:21:18 +0300

    modal

--- src/component/FirebaseAuth/StorageUploaderModal.jsx
+++ src/component/FirebaseAuth/StorageUploaderModal.jsx
@@ -6,18 +6,18 @@ import ImgCrop from 'antd-img-crop';
 export default function StorageUploader(props) {
   return (
     <>
-      <ImgCrop rotate>
-        <Upload
-          action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
-          listType='picture-card'
-          fileList={props.fileList}
-          onChange={props.onChange}
-          onPreview={props.onPreview}
-          type='file'
-        >
-          {props.fileList.length < 1 && '+ Upload'}
-        </Upload>
-      </ImgCrop>
+      {/* <ImgCrop rotate> */}
+      <Upload
+        action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
+        listType='picture-card'
+        fileList={props.fileList}
+        onChange={props.onChange}
+        onPreview={props.onPreview}
+        type='file'
+      >
+        {props.fileList.length < 1 && '+ Upload'}
+      </Upload>
+      {/* </ImgCrop> */}
       {/* <button onClick={handleUpload}>Upload</button> */}
     </>
   );
