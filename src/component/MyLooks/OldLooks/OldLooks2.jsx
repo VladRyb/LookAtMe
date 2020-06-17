a4613b8ba47c4246b9dc16392e72106f18693e98
Egor Zheltukhin <egorzel2011@gmail.com>
2020-06-17 12:20:19 +0300

    Stash

--- src/component/MyLooks/OldLooks/OldLooks2.jsx
+++ src/component/MyLooks/OldLooks/OldLooks2.jsx
@@ -25,76 +25,61 @@ function OldLooks2() {
   }
   return allLooks.map((element) => {
     return (
-      <div className='card mb-3 ' style={{ width: 450 }} id='loks'>
-        <div className='row no-gutters loks'>
+      <div className="card mb-3 " style={{ width: 450 }} id="loks">
+        <div className="row no-gutters loks">
           {element.ImgUrl ? (
-            <div className='col-md-4'>
-              <img src={element.ImgUrl} className='card-img' alt='photo' />
+            <div className="col-md-4">
+              <img src={element.ImgUrl} className="card-img" alt="photo" />
             </div>
           ) : (
-            <div className='col-md-4'>
-              <div id='container' className='flexChild rowParent'>
-                <div id='rowChild93188' className='flexChild columnParent'>
-                  <div id='columnChild38068' className='flexChild rowParent'>
-                    <div id='rowChild47552' className='flexChild'>
-                      <img
-                        src={element.head.imgUrl}
-                        className='card-img'
-                        alt='photo'
-                      />
+            <div className="col-md-4">
+              <div id="container" className="flexChild rowParent">
+                <div id="rowChild93188" className="flexChild columnParent">
+                  <div id="columnChild38068" className="flexChild rowParent">
+                    <div id="rowChild47552" className="flexChild">
+                      <img src={element.head.imgUrl} className="card-img" alt="photo" />
                     </div>
-                    <div id='rowChild999' className='flexChild'>
-                      <img
-                        src={element.body.imgUrl}
-                        className='card-img'
-                        alt='photo'
-                      />
+                    <div id="rowChild999" className="flexChild">
+                      <img src={element.body.imgUrl} className="card-img" alt="photo" />
                     </div>
                   </div>
-                  <div id='columnChild9857' className='flexChild rowParent'>
-                    <div id='rowChild89645' className='flexChild'>
-                      <img
-                        src={element.legs.imgUrl}
-                        className='card-img'
-                        alt='photo'
-                      />
+                  <div id="columnChild9857" className="flexChild rowParent">
+                    <div id="rowChild89645" className="flexChild">
+                      <img src={element.legs.imgUrl} className="card-img" alt="photo" />
                     </div>
-                    <div id='rowChild9992' className='flexChild'>
-                      <img
-                        src={element.feet.imgUrl}
-                        className='card-img'
-                        alt='photo'
-                      />
+                    <div id="rowChild9992" className="flexChild">
+                      <img src={element.feet.imgUrl} className="card-img" alt="photo" />
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           )}
-          <div className='col-md-8'>
-            <div className='card-body'>
-              <h5 className='card-title'>{element.name}</h5>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{element.name}</h5>
               {element.tags.map((element2) => {
-                return (
-                  <span className='tags badge badge-pill badge-dark'>
-                    {element2}
-                  </span>
-                );
+                return <span className="tags badge badge-pill badge-dark">{element2}</span>;
               })}
-              <div className='d-flex flex-row-reverse bd-highlight align-content-end links'>
-                <Link
-                  to={`/edit/${element.id}`}
-                  className='p-2 bd-highlight editLink'
-                >
-                  <i className='fa fa-pencil-square-o'></i>
+              <div className="d-flex flex-row-reverse bd-highlight align-content-end links">
+                <Link to={`/edit/${element.id}`} className="p-2 bd-highlight editLink">
+                  <i className="fa fa-pencil-square-o"></i>
                 </Link>
                 <span
-                  className='p-2 bd-highlight deleteLink'
+                  className="p-2 bd-highlight deleteLink"
                   onClick={() => {
                     deleteLook(element.id);
                   }}
                 >
-                  <i className='fa fa-trash-o'></i>
+                  <i className="fa fa-trash-o"></i>
+                </span>
+                <span
+                  className="p-2 bd-highlight deleteLink"
+                  onClick={() => {
+                    deleteLook(element.id);
+                  }}
+                >
+                  <i className="fa fa-trash-o">delete tags</i>
                 </span>
               </div>
             </div>
